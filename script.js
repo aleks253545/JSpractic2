600ae9de681ee3b13f8b10bbcdb8888e874cb0e0
aleks253545 <aleks253545@yandex.ru>
2020-01-30 14:16:13 +0300

    eslint

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let array = ['a','b','c','d','e','f','g','h'];
-function elephant (position1,position2){
-    let pos1=position1.split(''),
-    pos2=position2.split(''),
-    thisIndex;
 
-    array.forEach((element,index) => {
-        if (pos1[0]===element){
-            thisIndex=index;
-        }
-    });
-    if(8<pos1[1] || 1>pos1[1] || 8<pos2[1] || 1>pos2[1]){
-        return false
+function elephant(position1, position2) {
+  const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const pos1 = position1.split('');
+  const pos2 = position2.split('');
+  let thisIndex;
+  if (pos1[1] > 8 || pos1[1] < 1 || pos2[1] > 8 || pos2[1] < 1
+    || array.indexOf(pos1[0]) === -1 || array.indexOf(pos2[0]) === -1) {
+    console.log('неверное значение клеток');
+    return false;
+  }
+  array.find((element, index) => {
+    if (pos1[0] === element) {
+      thisIndex = index;
+      return true;
     }
-    if( +pos2[1]==+pos1[1]+2 || +pos2[1]+2==+pos1[1] ){
-        if( pos2[0]===array[thisIndex+1] ||  pos2[0]===array[thisIndex-1])
-        return true
-    }else{return false}
-    
+    return false;
+  });
+
+
+  if (+pos2[1] === +pos1[1] + 2 || +pos2[1] + 2 === +pos1[1]) {
+    if (pos2[0] === array[thisIndex + 1] || pos2[0] === array[thisIndex - 1]) return true;
+    return false;
+  } if (+pos2[1] === +pos1[1] + 1 || +pos2[1] + 1 === +pos1[1]) {
+    if (pos2[0] === array[thisIndex + 2] || pos2[0] === array[thisIndex - 2]) {
+      return true;
+    }
+    return false;
+  }
+  return false;
+
+// eslint глобальные переменные  find
 }
 
-console.log(elephant ('a3','b5'));
+console.log(elephant('a3', 'b5'));
+console.log(elephant('d3', 'c4'));
+console.log(elephant('a3', 'l8'));
